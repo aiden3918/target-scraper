afa8495128d54e590e6adfe74cf62b8624a04885
aiden3918 <liaiden3918@gmail.com>
2026-08-24 16:20:22 -0700

    added confirmation redundancy

--- main.py
+++ main.py
@@ -7,25 +7,22 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 import json
 
-def find_and_click_id(id_input, driver, maxWait=10):
+def find_id(id_input, driver, maxWait=10):
     btn = WebDriverWait(driver, maxWait).until(
         EC.element_to_be_clickable((By.ID, id_input))
     )
-    ActionChains(driver).click(btn).perform()
     return btn
 
-def find_and_click_text(x_path_input, driver, maxWait=10):
+def find_text(x_path_input, driver, maxWait=10):
     btn = WebDriverWait(driver, maxWait).until(
         EC.element_to_be_clickable((By.XPATH, x_path_input))
     )
-    ActionChains(driver).click(btn).perform()
     return btn
 
-def find_and_click_css_selector(css_input, driver, maxWait=10):
+def find_css_selector(css_input, driver, maxWait=10):
     btn = WebDriverWait(driver, maxWait).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, css_input))
     )
-    ActionChains(driver).click(btn).perform()
     return btn
 
 def find_and_enter_text_input_by_id(text_input, element_id, driver, maxWait=10):
@@ -34,17 +31,20 @@ def find_and_enter_text_input_by_id(text_input, element_id, driver, maxWait=10):
     )
     input.send_keys(text_input)
 
-def spam_click_button(input_button, end_condition, driver, clickDelay=0.1):
+def spam_click_button(input_button, end_condition, driver, second_end_condition = None, clickDelay=0.1):
     while True:
         try:
             # if end condition found, stop
             if WebDriverWait(driver, clickDelay).until(EC.presence_of_element_located(end_condition)):
                 break
+            if second_end_condition != None:
+                if WebDriverWait(driver, clickDelay).until(EC.presence_of_element_located(second_end_condition)):
+                    break
         except TimeoutException:
             # no end condition: keep clicking
             try:
-                button = driver.find_element(*input_button)
-                button.click()
+                ActionChains(driver).click(input_button).perform()
+                time.sleep(clickDelay)
             except ElementNotInteractableException:
                 pass
 
@@ -69,40 +69,52 @@ def buy():
         time.sleep(2)
         driver.quit()
 
+    # choose pickup, delivery, or shipping
+    purchase_option_str = "//span[text()='" + data["purchase-type"].capitalize() + "']"
+    purchase_option_btn = find_text(purchase_option_str, driver)
+    ActionChains(driver).click(purchase_option_btn).perform()
+
     # add to cart
     driver.execute_script("window.scrollBy(0, 650);")
     time.sleep(0.5)
-    find_and_click_css_selector("button[id*='addToCartButtonOrTextIdFor']", driver)
+    # spam until theres something in cart
+    add_to_cart_btn = find_css_selector("button[id*='addToCartButtonOrTextIdFor']", driver)
+    spam_click_button(add_to_cart_btn, (By.XPATH, "//a[text()='View cart & check out']"), driver)
     driver.get("https://www.target.com/cart")
 
     # sign in process
-    find_and_click_text("//button[text()='Sign in to check out']", driver)
+    check_out_btn = find_text("//button[text()='Sign in to check out']", driver)
+    spam_click_button(check_out_btn, (By.ID, "login"), driver)
 
     find_and_enter_text_input_by_id(data["username"], "username", driver)
-    find_and_click_id("login", driver)
-
-    find_and_click_text("//span[text()='Enter your password']", driver)
+    submit_user_btn = find_id("login", driver)
+    spam_click_button(submit_user_btn, (By.XPATH, "//span[text()='Enter your password']"), driver)
 
     find_and_enter_text_input_by_id(data["password"], "password", driver)
-    find_and_click_text("//button[text()='Sign in with password']", driver)
+    submit_pw_btn = find_text("//button[text()='Sign in with password']", driver)
+    spam_click_button(submit_pw_btn, (By.XPATH, "//a[text()='Skip']"), driver, (By.XPATH, "//a[text()='Skip']"))
 
     # mobile phone authentication
     try:
-        find_and_click_text("//a[text()='Skip']", driver)
+        skip_mobile_btn = find_text("//a[text()='Skip']", driver)
+        ActionChains(driver).click(skip_mobile_btn).perform()
         print("Skipped mobile phone authentication")
     except:
         print("Mobile phone authentication does not exist. Continuing...")
 
     # birthday (why is this even a thing)
     try:
-        find_and_click_id("EnrollmentMaybeLaterButton", driver)
+        skip_birthday_btn = find_id("EnrollmentMaybeLaterButton", driver)
+        ActionChains(driver).click(skip_birthday_btn).perform()
         print("Skipped birthday input")
     except:
         print("Birthday input does not exist. Continuing...")
 
     # Enter payment information
-    find_and_click_id("AddCreditDebitCellRadio", driver, 3)
-    find_and_click_id("saveCard", driver)
+    add_credit_checkbox = find_id("AddCreditDebitCellRadio", driver, 3)
+    ActionChains(driver).click(add_credit_checkbox).perform()
+    save_card_btn = find_id("saveCard", driver)
+    ActionChains(driver).click(save_card_btn).perform()
 
     find_and_enter_text_input_by_id(data["card-number"], "credit-card-number-input", driver)
     find_and_enter_text_input_by_id(data["expiration"], "credit-card-expiration-input", driver)
@@ -112,7 +124,7 @@ def buy():
     find_and_enter_text_input_by_id(data["address-line-1"], "billing-address-first-name-input", driver)
     # address line 2 if needed
     if data["address-line-2"] != "":
-        find_and_click_text("//button[text()='+ Address line 2']", driver)
+        find_text("//button[text()='+ Address line 2']", driver)
         find_and_enter_text_input_by_id(data["address-line-2"], "billing-address-line2-input", driver)
 
     find_and_enter_text_input_by_id(data["zip-code"], "billing-address-zip-code-input", driver)
@@ -120,13 +132,16 @@ def buy():
     # state should autocomplete
     find_and_enter_text_input_by_id(data["phone"], "billing-address-phone-input", driver)
 
-    find_and_click_text("//button[text()='Save and continue']", driver)
+    save_billing_btn = find_text("//button[text()='Save and continue']", driver)
+    spam_click_button(save_billing_btn, (By.CSS_SELECTOR, "button[disabled='']"))
 
     # place order
     time.sleep(0.5)
-    # find_and_click_text("//button[text()='Place your order']", driver)
+    # place_order_btn = find_text("//button[text()='Place your order']", driver)
+    # spam_click_button(place_order_btn, (By.XPATH, "//h1[text()='Thanks for your order!']"))
 
     # Wait to see the results, then close the browser safely
+    print("Order successfully placed; check email for confirmation details")
     time.sleep(1000)
     driver.quit()
 
@@ -139,3 +154,9 @@ if __name__ == '__main__':
 # Sign in to check out
 # username
 # https://www.target.com/p/mesh-binder-pouch-up-up/-/A-1011020920?preselect=94881673#lnk=sametab
+# ActionChains(driver).click(btn).perform()
+
+'''
+<button class="styles_btn__zZcJr styles_ndsButton__VgXft styles_md__N9Usy styles_filled__uq68y styles_fullWidth__ztP_d" type="button" data-test="placeOrderButton" fdprocessedid="5j8yif">Place your order</button>
+<button class="styles_btn__zZcJr styles_ndsButton__VgXft styles_md__N9Usy styles_filled__uq68y styles_fullWidth__ztP_d" type="button" data-test="placeOrderButton" disabled="">Place your order</button>
+'''
